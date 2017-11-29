011fb2abab96ef673af6fd11afc6b618991cc7d7
Karl <pvial@krypt.local>
2017-11-29 00:48:56 -0800

    MorseStation v0.1.2

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 from bleeps import Bleeps
 from KlassiKrypto import Morse
+import datetime
 
 class MorseStation:
-    def __init__(self, frequency=1000, wpm=18, timeunit=1.2, volume=1):
+    def __init__(self, frequency=1000, wpm=18, timeunit=1.2, volume=1, preamble=True):
         self.timeunit = timeunit
         self.wpm = wpm
         self.frequency = frequency
         self.volume = volume
+        self.preamble = preamble
 
     def farnsworth2_1(self):
         unit = float(self.timeunit / self.wpm)
@@ -17,8 +19,20 @@ class MorseStation:
         word_wait= ((unit * 7) - (unit * 3)) * 1000
         return dot_wait, dash_wait, element_wait, letter_wait, word_wait
 
-    def transmit(self, data, filename):
-        code = Morse().encode(data)
+    def transmit(self, data, filename, preamble="", eot="", msgnumber=0, station=""):
+        if self.preamble == True:
+            if preamble == "":
+               if msgnumber != 0:
+                   msgn = str(msgn)
+               else:
+                   msgn = ""
+               pre = station + "MSG" + msgn + "CK" + str(len(data)) + "CK" + datetime.datetime.now().strftime('%Y%M%d%H%M') + "BT"
+            if eot == "":
+               eot = "000"
+        else:
+            pre = ""
+            eot = ""
+        code = Morse().encode(pre+data+eot)
         bleeps = Bleeps()
         letters = code.split()
         dot_wait, dash_wait, element_wait, letter_wait, word_wait = self.farnsworth2_1()
